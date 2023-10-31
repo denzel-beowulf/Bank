59e6a88c243bc1518afefe22657c5b5036d58f76
Yohan Góngora <denzelhg86@gmail.com>
2023-10-31 19:12:41 -0400

    Init Commit,

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -5,8 +5,20 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_WithValidAmount_UpdatesBalances()
         {
+            //Arrange
+            double begginingBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Peter Wallace", begginingBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account Not Debited Correctly.");
         }
     }
 }
