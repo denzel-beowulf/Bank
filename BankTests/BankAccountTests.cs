6dd11b872bcb1e53d822c2d7fb064362bcf632d7
Yohan Góngora <denzelhg86@gmail.com>
2023-10-31 18:53:22 -0400

    Agregar archivos de proyecto.

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,12 @@
+using BankAccountNS;
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
