86225c6c64f83582330c649dcc60916df7279de8
Yohan Góngora <denzelhg86@gmail.com>
2023-11-01 00:02:22 -0400

    Writing tests

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -1,4 +1,6 @@
 using BankAccountNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace BankTests
 {
     [TestClass]
@@ -19,6 +21,44 @@ namespace BankTests
             //Assert
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account Not Debited Correctly.");
+        }
+
+        [TestMethod]
+        public void Debit_WhenTheAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double begginingBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Peter Wallace", begginingBalance);
+
+            //Act and Assert
+            try
+            {
+                Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+            }
+        }
+
+        [TestMethod]
+        public void Debit_WhenTheAmountIsGreaterThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double begginingBalance = 11.99;
+            double debitAmount = 500.00;
+            BankAccount account = new BankAccount("Mr. Peter Wallace", begginingBalance);
+
+            //Act and Assert
+            try
+            {
+                Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+            }
         }
     }
 }
